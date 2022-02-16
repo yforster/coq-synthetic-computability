a5a603bd5f6bbd4597bc1a998036211c917455a2
Yannick Forster <yannick.forster@inria.fr>
2022-02-16 09:40:46 +0100

    factor using enumerability of basic neighbourhoods

--- Synthetic/Baire.v
+++ Synthetic/Baire.v
@@ -44,6 +44,9 @@ Definition prefix (l : list nat) (β : B) :=
 Variable is_prefix : list nat -> B -> bool.
 Variable is_prefix_spec : forall l β, prefix l β <-> is_prefix l β = true.
 
+Lemma is_prefix_spec' : forall l β, prefix l β -> is_prefix l β = true.
+Proof. eapply is_prefix_spec. Qed.
+
 Definition thefirst : forall {X}, (nat -> option X) ↛ X :=
   fun X f =>
     bind (mu_tot (fun n => match f n with None => false | _ => true end))
@@ -72,11 +75,66 @@ Proof.
     f_equal; eauto.
 Qed.
 
+Definition extend L x :=
+  nth x L 0.
+
+Definition neighbourhood (f : B -> nat) M : list nat * nat -> Prop :=
+  fun '(L, m) => M (extend L) <= length L /\ forall α, prefix L α -> M α <= length L -> f α = m.
+
+Definition enumerable {X} (p : X -> Prop) :=
+  exists f, forall x, p x <-> exists n : nat, f n = Some x.
+
+Lemma map_nth_seq {X} (x0 : X) l :
+  map (fun n => nth n l x0) (seq 0 (length l)) = l.
+Proof.
+  induction l using rev_ind.
+  - reflexivity.
+  - cbn. rewrite app_length, seq_app, map_app.
+    cbn. rewrite app_nth2. 2:lia.
+    rewrite sub_diag. cbn. f_equal.
+    rewrite <- IHl at 2.
+    eapply map_ext_in.
+    intros ? [_ ?] % in_seq. cbn in *.
+    now rewrite app_nth1.
+Qed.
+
+Definition pref {X} (f : nat -> X) n :=
+  map f (seq 0 n).
+
+Lemma enumerable_neigh :
+  forall F M : B -> nat,
+    is_modulus M F ->
+    enumerable (neighbourhood F M).
+Proof.
+  intros F M Hcont.
+  pose (μ f := map f (seq 0 (M f))).
+
+  exists (fun n => let L_n := enum n in
+           if  M (extend L_n) <=? length L_n then
+             Some (L_n, F (extend L_n))
+           else None).
+  intros [L v]. split.
+  - intros Hneigh. cbn.
+    destruct (enum_spec L) as [n ->].
+    exists n. destruct Hneigh as [H1 H2].
+    rewrite Compare_dec.leb_correct; eauto.
+    repeat f_equal. red in Hcont.
+    eapply H2. red. eapply map_nth_seq. eauto.
+  - cbn. intros [n H].
+    destruct (leb_spec (M (extend (enum n))) (length (enum n))); try congruence.
+    inversion H; subst; clear H. split; [ eauto | ].
+    intros. eapply Hcont.
+    eapply Forall_forall. intros x [_ Hx] % in_seq.
+    unfold extend. red in H. rewrite <- H.
+    eapply app_seq_eq. lia.
+Qed.
+
 Definition apply (α : B) (β : B) : part nat :=
-  thefirst (fun i => let (l, m) := dec (α i) in
-        if is_prefix l β
-        then Some m
-        else None).
+  thefirst (fun i => match α i with 0 => None
+                            | S αi => let (l, m) := dec αi in
+                                     if is_prefix l β
+                                     then Some m
+                                     else None end).
 
 Lemma apply_spec :
   forall F M : B -> nat,
@@ -85,74 +143,61 @@ Lemma apply_spec :
     exists α, forall β, apply α β =! F β.
 Proof.
   intros F M Hcont HcontM.
-  pose (μ f := map f (seq 0 (M f))).
 
-  exists (fun n => let L_n := enum n in
-           let f_n x := nth x L_n 0 in
-           code (μ f_n) (F f_n)).
-  intros β.
-  unfold apply. eapply thefirst_spec; setoid_rewrite dec_spec.
+  destruct (enumerable_neigh F M Hcont) as [α Hα].
+  exists (fun n => match α n with None => 0 | Some (x1, x2) => S (code x1 x2) end).
+  intros β. unfold apply. eapply thefirst_spec.
   2:{
     destruct (HcontM β) as [m Hm].
-    destruct (enum_spec (map β (seq 0 (M β + m)))) as [n Hn].
-    exists n. rewrite <- !Hn. clear Hn.
-    assert (is_prefix (μ (fun x : nat => nth x (map β (seq 0 (M β + m))) 0)) β = true) as ->. {
-      eapply is_prefix_spec. red.
-      unfold μ. rewrite map_length, seq_length.
-      eapply map_seq_eq. intros i [_ Hi].
-      eapply app_seq_eq.
-      eapply lt_le_trans. eauto.
-      rewrite <- Hm. lia.
-      eapply Forall_forall. intros x Hx % in_seq.
-      eapply app_seq_eq. lia.
-    }
-    f_equal. unfold μ.
-    symmetry. eapply (Hcont β).
-    eapply Forall_forall.
-    intros x Hx % in_seq.
-    eapply app_seq_eq. lia.
+    destruct (Hα (map β (seq 0 (M β + m)), F β)) as [[n Hn] _].
+    - red. rewrite map_length, seq_length.
+      erewrite <- Hm.
+      2:{ eapply Forall_forall. intros x [_ Hx] % in_seq.
+          eapply app_seq_eq. lia. }
+      split. lia.
+      intros. eapply Hcont.
+      eapply Forall_forall. intros x [_ Hx] % in_seq.
+      red in H. rewrite map_length, seq_length in H.
+      eapply ext_in_map. exact H.
+      eapply in_seq. lia.
+    - exists n. rewrite Hn. rewrite dec_spec.
+      rewrite is_prefix_spec'. reflexivity.
+      red. now rewrite map_length, seq_length.
   }
   clear HcontM.
   intros n1 n2 x1 x2 H1 H2.
+  destruct (α n1) as [[l1 v1] | ] eqn:Eα1; try congruence.
+  destruct (α n2) as [[l2 v2] | ] eqn:Eα2; try congruence.
+  rewrite dec_spec in *.
   destruct is_prefix eqn:E1; inversion H1; subst; clear H1.
-  destruct (is_prefix (μ (fun x : nat => nth x (enum n2) 0)) β)
+  destruct (is_prefix l2 β)
            eqn:E2; inversion H2; subst; clear H2.
   eapply is_prefix_spec in E1, E2.
   red in E1, E2.
-  unfold μ in *. rewrite map_length, seq_length in *.
-  assert ((M (fun x : nat => nth x (enum n1) 0) <= (M (fun x : nat => nth x (enum n2) 0)))
-          \/ (M (fun x : nat => nth x (enum n2) 0) <= (M (fun x : nat => nth x (enum n1) 0))))
+  assert (neighbourhood F M (l1, x1)) as [H1 H1'] by firstorder.
+  assert (neighbourhood F M (l2, x2)) as [H2 H2'] by firstorder.
+  assert (length l1 <= length l2 \/ length l2 <= length l1)
          as [l | l] by lia.
   - eapply le_exists_sub in l as [k [Hk _]].
     rewrite add_comm in Hk.
-    eapply Hcont.
-    rewrite Hk in *. rewrite seq_app in E2. cbn in E2.
-    rewrite !map_app in E2. eapply app_length_inv in E2 as [E2 E3].
-    2: now rewrite !map_length, !seq_length.
-    rewrite <- map_seq_eq in E2.
-    eapply Forall_forall.
-    intros x [_ Hx] % in_seq.
-    assert (0 <= x < 0 + M (fun x : nat => nth x (enum n1) 0)) as Hx'' by lia.
-    eapply E2 in Hx'' as Hx'.
-    rewrite <- Hx'.
-    rewrite <- map_seq_eq in E1.
-    rewrite <- map_seq_eq in E3.
-    symmetry. eapply E1. eauto.
-  - (* symmetric case *) symmetry. rename E1 into EE. rename E2 into E1.
+    rewrite <- (H1' (extend l1)), <- (H2' (extend l2)); try lia.
+    2, 3: eapply map_nth_seq.
+    eapply Hcont. eapply Forall_forall. intros x [_ Hx] % in_seq.
+    cbn in *.
+    rewrite <- E1, <- E2. unfold extend.
+    rewrite Hk. rewrite seq_app, map_app.
+    rewrite app_nth1. 2: rewrite map_length, seq_length; lia.
+    reflexivity.
+  - symmetry. rename E1 into EE. rename E2 into E1.
     rename EE into E2.
-    eapply le_exists_sub in l as [k [Hk _]].
+     eapply le_exists_sub in l as [k [Hk _]].
     rewrite add_comm in Hk.
-    eapply Hcont.
-    rewrite Hk in *. rewrite seq_app in E2. cbn in E2.
-    rewrite !map_app in E2. eapply app_length_inv in E2 as [E2 E3].
-    2: now rewrite !map_length, !seq_length.
-    rewrite <- map_seq_eq in E2.
-    eapply Forall_forall.
-    intros x [_ Hx] % in_seq.
-    assert (0 <= x < 0 + M (fun x : nat => nth x (enum n2) 0)) as Hx'' by lia.
-    eapply E2 in Hx'' as Hx'.
-    rewrite <- Hx'.
-    rewrite <- map_seq_eq in E1.
-    rewrite <- map_seq_eq in E3.
-    symmetry. eapply E1. eauto.
+    rewrite <- (H1' (extend l1)), <- (H2' (extend l2)); try lia.
+    2, 3: eapply map_nth_seq.
+    eapply Hcont. eapply Forall_forall. intros x [_ Hx] % in_seq.
+    cbn in *.
+    rewrite <- E1, <- E2. unfold extend.
+    rewrite Hk. rewrite seq_app, map_app.
+    rewrite app_nth1. 2: rewrite map_length, seq_length; lia.
+    reflexivity.
 Qed.
